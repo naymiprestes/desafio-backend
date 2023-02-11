7669bb951bb17611e7bd9dd5747ced422be30d1b
naymi <naymi.prestes1997@gmail.com>
2023-02-11 02:14:58 -0400

    upload do arquivo CNAB

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
-import pandas as pd
-import numpy as np
-import psycopg2 as pg
+import os
+from flask import Flask, render_template, request
+import sqlite3
+from unicodedata import normalize
 
-conexao = pg.connect(
-    database = 'db_cnab',
-    port = 5432,
-    user = 'naymi',
-    password='1234'
-)
+app = Flask(__name__)
 
-columns=['Tipo', 'Data',  'Valor', 'CPF', 'Cartão', 'Hora', 'Dono da loja', 'Nome_loja']
-data = pd.read_fwf('CNAB.txt', names = columns , widths=[1,8,10,11,12,6,14,19], header=None, index_col=None)
-print(data)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload", methods=['POST'])
+def dados():
+
+    arquivo = request.files.get('arquivo')
+    arquivo.save(os.path.join('./', 'database.txt'))
+
+    arquivo_aberto = open('database.txt')
+    dados = []
+    estabelecimentos = []
+    transacoes = []
+    saldos = []
+
+    def valor(valor_transacao, tipo):
+        if tipo in ['2', '3', '9']:
+            return valor_transacao * (-1)
+        else:
+            return valor_transacao
+
+    def palavras(palavra):
+        string = normalize('NFD', palavra)
+        string = string.encode('ASCII', 'ignore')
+        string = string.decode('ASCII')
+        return string
+    
+    for item in arquivo_aberto:
+        dados.append((item[0:1], item[1:9], valor(float(item[9:19])/100.00, item[0:1]), item[19:30],
+                      item[30:42], item[42:48], palavras(item[48:62].strip()), palavras(item[62:82].strip()),))
+        
+
+    banco = sqlite3.connect('db.sqlite')
+    cursor = banco.cursor()
+    cursor.execute('CREATE TABLE dados (tipo text, data text, valor real, cpf text, cartao text, hora text, dono text, loja text)')
+    cursor.executemany("INSERT INTO dados(tipo, data, valor, cpf, cartao, hora, dono, loja) VALUES(?,?,?,?,?,?,?,?)", dados)
+    banco.commit()
+    cursor.execute('SELECT DISTINCT loja FROM dados')
 
 
-value = data.groupby('Tipo').agg({'Valor':'sum', 'Nome_loja': 'sum'})
+    for item in cursor.fetchall():
+        estabelecimentos.append(item[0])
 
-value = value.sort_values(by=['Valor'], 
-                      axis=0, 
-                      ascending=True,  
-                      ignore_index=False)
+    for item in estabelecimentos:
+        data = cursor.execute('SELECT * FROM dados WHERE loja = ?', (item,))
+        transacoes.append(data.fetchall())
 
-value = value.reset_index()
-print(value)
+    for item in estabelecimentos:
+        cursor.execute('SELECT SUM(valor) FROM dados WHERE loja = ?', (item,))
+        saldo = cursor.fetchall()[0][0]
+        saldos.append(saldo)
+
+
+         
+    banco.close()
+    arquivo_aberto.close()
+
+    return render_template('upload.html', transacoes=transacoes, saldos=saldos)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
